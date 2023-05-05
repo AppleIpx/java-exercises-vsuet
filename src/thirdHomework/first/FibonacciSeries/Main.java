40198a1e103920e297e66fe074cf30b3dd853f46
Eugeniy <zhenya.0295@mail.ru>
2023-05-05 10:43:52 +0300

    added HomeTask 3

---
+++ src/thirdHomework/first/FibonacciSeries/Main.java
@@ -0,0 +1,13 @@
+package thirdHomework.first.FibonacciSeries;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число из ряда Фибоначчи");
+        int userDit = scanner.nextInt();
+        Fibonacci fibonacci = new Fibonacci(userDit);
+        System.out.println(fibonacci);
+    }
+}
